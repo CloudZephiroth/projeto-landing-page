b7f5a9eb59e2ba1539895172cb5aa790e6163a7e
Wagner Oliver23 <devwagneroliver@gmail.com>
2023-07-21 12:46:32 -0300

    Versao Inicial

---
+++ index.js
@@ -0,0 +1,20 @@
+var leonardo = window.document.getElementById("leonardo");
+var samantha = window.document.getElementById("samantha");
+var bruna = window.document.getElementById("bruna");
+
+var setaEsquerda = window.document.getElementById("seta_esquerda");
+var setaDireita = window.document.getElementById("seta_direita");
+
+function RolarParaDireita(){
+  leonardo.style = "display:none"
+  bruna.style="display:flex"
+  setaDireita.style = "display:none"
+  setaEsquerda.style = "display:flex; margin-top:55px"
+}
+
+function RolarParaEsquerda(){
+  leonardo.style = "display:flex"
+  bruna.style="dsiplay:none"
+  setaDireita.style = "display:flex; margin-top:55px"
+  setaEsquerda.style = "display:none"
+}
